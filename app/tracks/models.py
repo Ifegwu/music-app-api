0c2cb3d5dbe180cc12ffad16d8fad1dc00c09453
Ifegwu <agbanyimdaniel29@gmail.com>
2021-02-23 15:33:29 +0100

    enforcing https in django model

--- app/tracks/models.py
+++ app/tracks/models.py
@@ -1,13 +1,17 @@
 from django.db import models
 # from django.contrib.auth import get_user_model
 from app.registration.models import User
+from django.core.validators import RegexValidator
+URL_VALIDATOR_MESSAGE = 'Not a valid URL.'
+URL_VALIDATOR = RegexValidator(regex='/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/', message=URL_VALIDATOR_MESSAGE)
+
 
 
 class Track(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField(blank=True)
-    url = models.URLField()
-    avarta = models.URLField(blank=True, default='DEFAULT VALUE')
+    url = models.URLField(validators=[URL_VALIDATOR])
+    avarta = models.URLField(blank=True, validators=[URL_VALIDATOR])
     created_at = models.DateTimeField(auto_now_add=True)
     posted_by = models.ForeignKey(
         # get_user_model(),
